4c4c45dc28c21b6e1eb1d2012e88e513ef9b7aef
Laketsho187 <03230152.gcbs@rub.edu.bt>
2024-03-07 14:13:21 +0600

    Positive

---
+++ positiveNegative.py
@@ -0,0 +1,29 @@
+# objective
+# Create a program that takes in user input
+# determine if the number is positive or negative
+# print 'ur number id positive' or 'ur number is negative'
+
+# If else
+# print()
+# input()
+
+# 3 minutes
+
+# break down the problem
+# Take in user input
+        # Check the Type of the input
+        # if the type is string, how  do you convert it to an int?
+2. # check if the number is positive or negative or zero
+        #need to use if else statement
+        # ! a == b (is it equal to)
+        # ! a != b (Not equal to)
+        # ypu will be comparing numbers and not string
+# Print the result
+
+# Solution
+# Get user input
+userinput = input()
+
+userInputType = type(userinput)
+print('The type of user input is:', userinput)
+
