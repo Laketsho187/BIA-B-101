26d5c929d49a683d4ca039fad3e6644f34b25c59
Laketsho187 <03230152.gcbs@rub.edu.bt>
2024-03-14 15:20:57 +0600

    functions

---
+++ function.py
@@ -0,0 +1,25 @@
+def greet_Nima():
+    # do some calculation code
+    # complex stuff
+    print('hello Nima')
+
+user_input = 'Nima'
+if user_input == "Nima":
+    greet_Nima()
+
+def greet():
+    # do some calculation code
+    # complex stuff
+    print('hello')
+
+def add_number(num1, num2):
+    sum = num1 + num2
+    print('adding numbers')
+    return sum
+
+userNumber = 10
+userNumber2 = 20
+
+
+returned_sum = add_number(userNumber, userNumber2)
+print(returned_sum)
